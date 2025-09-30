ca61067b58a9e8130a4361fdf4a32e92c1a1bd67
anic17 <58483910+anic17@users.noreply.github.com>
2025-09-30 10:55:55 +0200

    LexaCount v1.3 - Bug fixes, code improvement, `-x` parameter

--- lexacount.c
+++ lexacount.c
@@ -6,7 +6,7 @@
         of source code in software projects. It provides various options to adapt to
         different needs, such as excluding lines with only brackets or using table output.
 
-    Copyright (c) 2024 anic17 Software
+    Copyright (c) 2025 anic17 Software
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -31,19 +31,26 @@
 
 #include "lexacount.h"
 
-int is_quote(char *quotes_str, size_t qlen, int chr)
+int in_string(char *s, int chr)
 {
-    for (size_t i = 0; i < qlen; i++)
-    {
-        if (chr == quotes_str[i])
-            return true;
-    }
-    return false;
+    return strrchr(s, chr) != NULL;
+}
+
+int strlen_n(const char *s)
+{
+    if (!s)
+        return 0;
+    return strlen(s);
 }
 
 void show_version()
 {
     printf("LexaCount v%s - Count source lines of code.\n", lexacount_version);
+}
+
+void show_copyright()
+{
+    printf("Copyright (c) 2025 anic17 Software\n");
 }
 
 void help()
@@ -57,8 +64,11 @@ void help()
         " -h, --help     Display this help menu\n"
         " -l, --list     Load file names from a file\n"
         " -t, --table    Print the output using tables\n"
-        " -v, --version  Display program version\n\n"
-        "Copyright (c) 2024 anic17 Software\n");
+        " -v, --version  Display program version\n"
+        " -x, --exclude  Exclude files that failed to open\n\n"
+
+    );
+    show_copyright();
 }
 
 void missing_param(char *s)
@@ -67,59 +77,64 @@ void missing_param(char *s)
     exit(1);
 }
 
-size_t count_loc(char *filename, char **comments, char *quotes, char *brackets, size_t *file_loc, size_t *file_comment, size_t *file_blank, size_t *file_bracket, bool excludeBrackets)
+size_t count_loc(char *filename, item_count *ic, text_fields *tf, param *p, int *result)
 {
-    *file_loc = 0;
-    *file_comment = 0;
-    *file_blank = 0;
-    *file_bracket = 0;
+    ic->loc = 0;
+    ic->comment = 0;
+    ic->blank = 0;
+    ic->bracket = 0;
+
     filename[strcspn(filename, "\r\n")] = '\0';
     if (filename[0] == '\0')
     {
+        *result = -EINVAL;
         return 0;
     }
+
     FILE *count_fp = fopen(filename, "rb");
     if (!count_fp)
     {
-        fprintf(stderr, "%s: %s\n", filename, strerror(errno));
+        if (!p->excludeFiles)
+            fprintf(stderr, "Error: %s: %s\n", filename, strerror(errno));
+        *result = -errno;
         return 0;
     }
     bool isLineBlank = true;
     bool isLineOnlyBrackets = false;
     bool isFileEmpty = true;
-    int chr = 0;
+    bool quoteStatus = false;
     size_t comment_idx = 0;
-    int quoteStatus = 0; // Status of the quotes
-    size_t qlen = strlen(quotes);
-    size_t blen = strlen(brackets);
-    size_t clen = strlen(comments[0]);
+
+    size_t multi_comment_idx = 0;
+    int chr = 0;
+
+    size_t clen[3] = {strlen_n(tf->comments[0]), strlen_n(tf->comments[1]), strlen_n(tf->comments[2])};
 
     while ((chr = fgetc(count_fp)) != EOF)
     {
 
         isFileEmpty = false;
-        if (is_quote(quotes, qlen, chr))
-        {
+        if (in_string(tf->quotes, chr))
             quoteStatus ^= chr;
-        }
+
         if (isLineBlank && !quoteStatus)
         {
-            if (comment_idx >= clen - 1)
+            if (comment_idx >= clen[0] - 1)
             {
-                (*file_comment)++;
+                ic->comment++;
                 comment_idx = 0;
                 continue;
             }
-            if (chr == comments[0][comment_idx])
+            if (chr == tf->comments[0][comment_idx])
             {
                 comment_idx++;
                 continue;
             }
         }
 
-        if ((!isspace(chr) || (!is_bracket(brackets, blen, chr) && excludeBrackets)))
+        if ((!isspace(chr) || (!in_string(tf->brackets, chr) && p->excludeBrackets)))
         {
-            if (is_bracket(brackets, blen, chr) && excludeBrackets)
+            if (in_string(tf->brackets, chr) && p->excludeBrackets)
             {
                 isLineOnlyBrackets = true;
             }
@@ -135,15 +150,11 @@ size_t count_loc(char *filename, char **comments, char *quotes, char *brackets, 
         if (chr == '\n')
         {
             if (isLineOnlyBrackets && isLineBlank)
-            {
-                (*file_bracket)++;
-            }
+                ic->bracket++;
             else if (isLineBlank)
-            {
-                (*file_blank)++;
-            }
+                ic->blank++;
 
-            (*file_loc)++;
+            ic->loc++;
             isLineBlank = true;
             isLineOnlyBrackets = false;
             quoteStatus = 0;
@@ -154,18 +165,17 @@ size_t count_loc(char *filename, char **comments, char *quotes, char *brackets, 
     if (!isFileEmpty)
     {
         if (isLineOnlyBrackets && isLineBlank)
-        {
-            (*file_bracket)++;
-        }
+            ic->bracket++;
         else if (isLineBlank)
-        {
-            (*file_blank)++;
-        }
-        (*file_loc)++;
+            ic->blank++;
+
+        ic->loc++;
     }
 
     fclose(count_fp);
-    return *file_loc;
+
+    *result = 1; // > 0 means success, == 0 means error
+    return ic->loc;
 }
 
 void print_table_header(const char *formatspec, bool excludeBrackets)
@@ -175,70 +185,57 @@ void print_table_header(const char *formatspec, bool excludeBrackets)
     printf(" | %-10s | %-10s | %-10s |", "Code lines", "Blank", "Comments");
 
     if (excludeBrackets)
-    {
-        printf(" %-15s |", "Brackets only");
-    }
+        printf(" %-10s |", "Brackets");
 
     printf("\n");
 }
 
-void print_table_separator(size_t flen, bool excludeBrackets)
+void print_table_separator(size_t flen, param *p)
 {
     printf("|");
     for (size_t k = 0; k < flen + 2; k++)
-    {
-        printf("-");
-    }
+        putchar('-');
+
     printf("|------------|------------|------------|");
 
-    if (excludeBrackets)
-    {
-        printf("-----------------|");
-    }
+    if (p->excludeBrackets)
+        printf("------------|");
+
     printf("\n");
 }
 
-void print_table_row(const char *formatspec, const char *filename, size_t lines, size_t blanks, size_t comments, size_t brackets, bool excludeBrackets)
+void print_table_row(const char *formatspec, const char *filename, item_count *ic, param *p)
 {
     printf("| ");
     printf(formatspec, filename);
-    printf(" | %-10zu | %-10zu | %-10zu |", excludeBrackets ? (lines - blanks - comments - brackets) : (lines - blanks - comments), blanks, comments);
+    printf(" | %-10zu | %-10zu | %-10zu |", ic->loc - ic->blank - ic->comment - ic->bracket * (!!p->excludeBrackets), ic->blank, ic->comment);
 
-    if (excludeBrackets)
-    {
-        printf(" %-15zu |", brackets);
-    }
+    if (p->excludeBrackets)
+        printf(" %-10zu |", ic->bracket);
+
     printf("\n");
 }
 
-void display_lines(const char *formatspec, const char *filename, size_t file_loc, size_t file_blank, size_t file_comment, size_t file_bracket, bool tableOutput, bool excludeBrackets)
+void display_lines(const char *formatspec, const char *filename, item_count *ic, param *p)
 {
-    if (tableOutput)
-    {
-        print_table_row(formatspec, filename, file_loc, file_blank, file_comment, file_bracket, excludeBrackets);
-    }
+    if (p->tableOutput)
+        print_table_row(formatspec, filename, ic, p);
     else
     {
-        printf("%s: %zu lines", filename, excludeBrackets ? (file_loc - file_blank - file_comment - file_bracket) : (file_loc - file_blank - file_comment));
+        printf("%s: %zu code lines", filename, ic->loc - ic->blank - ic->comment - ic->bracket * (!!p->excludeBrackets));
 
-        if (file_blank > 0 || file_comment > 0 || file_bracket > 0)
+        if (ic->blank > 0 || ic->comment > 0 || ic->bracket > 0)
         {
-            printf(" (excluding");
+            printf(" (and");
 
-            if (file_blank > 0)
-            {
-                printf(" %zu blank line%s", file_blank, (file_blank == 1) ? "" : "s");
-            }
+            if (ic->blank > 0)
+                printf(" %zu blank line%s", ic->blank, (ic->blank == 1) ? "" : "s");
 
-            if (file_comment > 0)
-            {
-                printf("%s%zu comment%s", (file_blank > 0) ? " and " : " ", file_comment, (file_comment == 1) ? "" : "s");
-            }
+            if (ic->comment > 0)
+                printf("%s%zu comment%s", (ic->blank > 0) ? " and " : " ", ic->comment, (ic->comment == 1) ? "" : "s");
 
-            if (file_bracket > 0 && excludeBrackets)
-            {
-                printf("%s%zu line%s with only brackets", ((file_blank > 0) || (file_comment > 0)) ? " and " : " ", file_bracket, (file_bracket == 1) ? "" : "s");
-            }
+            if (ic->bracket > 0 && p->excludeBrackets)
+                printf("%s%zu line%s with only brackets", ((ic->blank > 0) || (ic->comment > 0)) ? " and " : " ", ic->bracket, (ic->bracket == 1) ? "" : "s");
 
             printf(")");
         }
@@ -248,9 +245,7 @@ void display_lines(const char *formatspec, const char *filename, size_t file_loc
 
 int main(int argc, char *argv[])
 {
-    char quotechars[] = "'\"";
-    char bracketchars[] = "{}[]()";
-    if (argc < 2 || !strcmp(argv[1], "--help") || !strcmp(argv[1], "/?"))
+    if (argc < 2 || !strcmp(argv[1], "--help") || !strcmp(argv[1], "/?") || !strcmp(argv[1], "-h"))
     {
         help();
         return 0;
@@ -258,15 +253,18 @@ int main(int argc, char *argv[])
     char *loc_file = argv[1];
     FILE *list;
 
-    char *comments[] = {"//"};
+    char *comments[] = {"//", "/*", "*/"};
 
     char **fnames = calloc(argc - 1, sizeof(char *)), **list_fnames = calloc(argc - 1, sizeof(char *));
     size_t fname_count = 0, list_fname_count = 0;
 
     size_t longest_fname = 0;
 
-    bool excludeBrackets = false;
-    bool tableOutput = false;
+    item_count ic = {0, 0, 0, 0}, total = {0, 0, 0, 0};
+
+    text_fields tf = {comments, "{}[]();", "'\""};
+    param p = {NULL, false, false};
+
     for (int i = 1; i < argc; i++)
     {
         if (!strcmp(argv[i], "-l") || !strcmp(argv[i], "--list"))
@@ -275,12 +273,11 @@ int main(int argc, char *argv[])
             {
                 if (argv[i] != NULL)
                 {
-                    list_fnames[list_fname_count] = calloc(strlen(argv[i + 1]) + 1, sizeof(char));
-                    memcpy(list_fnames[list_fname_count], argv[i + 1], strlen(argv[i + 1]));
-                    if (strlen(list_fnames[list_fname_count]) > longest_fname)
-                    {
-                        longest_fname = strlen(list_fnames[list_fname_count]);
-                    }
+                    list_fnames[list_fname_count] = calloc(strlen_n(argv[i + 1]) + 1, sizeof(char));
+                    memcpy(list_fnames[list_fname_count], argv[i + 1], strlen_n(argv[i + 1]));
+                    if (strlen_n(list_fnames[list_fname_count]) > longest_fname)
+                        longest_fname = strlen_n(list_fnames[list_fname_count]);
+
                     list_fname_count++;
                 }
                 i++;
@@ -293,16 +290,20 @@ int main(int argc, char *argv[])
 
         else if (!strcmp(argv[i], "-b") || !strcmp(argv[i], "--bracket"))
         {
-            excludeBrackets = true;
+            p.excludeBrackets = true;
         }
         else if (!strcmp(argv[i], "-t") || !strcmp(argv[i], "--table"))
         {
-            tableOutput = true;
+            p.tableOutput = true;
+        }
+        else if (!strcmp(argv[i], "-x") || !strcmp(argv[i], "--exclude"))
+        {
+            p.excludeFiles = true;
         }
         else if (!strcmp(argv[i], "-v") || !strcmp(argv[i], "--version"))
         {
             show_version();
-            printf("\nCopyright (c) 2024 anic17 Software\n");
+            show_copyright();
             exit(0);
         }
 
@@ -310,96 +311,102 @@ int main(int argc, char *argv[])
         {
             if (argv[i] != NULL)
             {
-                fnames[fname_count] = calloc(strlen(argv[i]) + 1, sizeof(char));
-                memcpy(fnames[fname_count], argv[i], strlen(argv[i]));
-                if (strlen(fnames[fname_count]) > longest_fname)
-                {
-                    longest_fname = strlen(fnames[fname_count]);
-                }
+                fnames[fname_count] = calloc(strlen_n(argv[i]) + 1, sizeof(char));
+                memcpy(fnames[fname_count], argv[i], strlen_n(argv[i]));
+                if (strlen_n(fnames[fname_count]) > longest_fname)
+                    longest_fname = strlen_n(fnames[fname_count]);
+
                 fname_count++;
             }
         }
     }
 
-    size_t total_loc = 0, total_comment_loc = 0, total_blank_loc = 0, total_bracket_loc = 0;
-    size_t file_loc = 0, file_comment_loc = 0, file_blank_loc = 0, file_bracket_loc = 0;
-    size_t file_count = 0;
+    size_t count = 0;
 
     char filename[512];
 
     if (longest_fname < 11)
-    {
         longest_fname = 11;
-    }
 
     char *fspec = calloc(sizeof(char), longest_fname + 10);
     snprintf(fspec, longest_fname, "%%-%zus", longest_fname);
 
-    for (size_t i = 0; i < list_fname_count; i++)
+    int count_retcode = 0;
+
+    for (size_t i = 0; i < list_fname_count; i++) // Load all filenames from all the list files into memory
     {
         list = fopen(list_fnames[i], "rb");
         if (!list)
         {
-            fprintf(stderr, "%s: %s", loc_file, strerror(errno));
-            return errno;
+            if (!p.excludeFiles)
+                fprintf(stderr, "Error: %s: %s", list_fnames[i], strerror(errno)); // List file error
+            return -errno;
         }
         while (fgets(filename, sizeof filename, list)) // If someone knows a better way to avoid incorrect padding, let me know
         {
-            if (strlen(filename) > longest_fname)
+            if (strlen_n(filename) > longest_fname)
             {
-                longest_fname = strlen(filename);
+                longest_fname = strlen_n(filename);
                 snprintf(fspec, longest_fname, "%%-%zus", longest_fname);
             }
         }
         fclose(list);
     }
 
-    if (tableOutput)
+    if (p.tableOutput)
     {
-        print_table_header(fspec, excludeBrackets);
+        print_table_header(fspec, p.excludeBrackets);
         if (list_fname_count == 0)
-            print_table_separator(longest_fname, excludeBrackets);
+            print_table_separator(longest_fname, &p);
     }
 
-    for (size_t i = 0; i < list_fname_count; i++)
+    for (size_t i = 0; i < list_fname_count; i++) // Process the filenames from the list files
     {
         list = fopen(list_fnames[i], "rb");
         if (!list)
         {
-            fprintf(stderr, "%s: %s", loc_file, strerror(errno));
+            if (!p.excludeFiles)
+                fprintf(stderr, "Error: %s: %s", list_fnames[i], strerror(errno)); // Files inside list file error
             return errno;
         }
-        if (tableOutput)
-            print_table_separator(longest_fname, excludeBrackets);
+        if (p.tableOutput)
+            print_table_separator(longest_fname, &p);
 
-        while (fgets(filename, sizeof filename, list))
+        while (fgets(filename, sizeof filename, list)) // Process each individual file from each list
         {
-            count_loc(filename, comments, quotechars, bracketchars, &file_loc, &file_comment_loc, &file_blank_loc, &file_bracket_loc, excludeBrackets);
-            display_lines(fspec, filename, file_loc, file_blank_loc, file_comment_loc, file_bracket_loc, tableOutput, excludeBrackets);
-            file_count++;
-            total_loc += file_loc;
-            total_comment_loc += file_comment_loc;
-            total_blank_loc += file_blank_loc;
-            total_bracket_loc += file_bracket_loc;
+
+            count_loc(filename, &ic, &tf, &p, &count_retcode);
+            if (count_retcode >= 0)
+            {
+
+                total.loc += ic.loc;
+                total.comment += ic.comment;
+                total.blank += ic.blank;
+                total.bracket += ic.bracket;
+                display_lines(fspec, filename, &ic, &p);
+            }
+
+            count++;
         }
         fclose(list);
     }
 
-    for (size_t i = 0; i < fname_count; i++)
+    int valid_file = 0;
+    for (size_t i = 0; i < fname_count; i++) // Process normal files
     {
-        count_loc(fnames[i], comments, quotechars, bracketchars, &file_loc, &file_comment_loc, &file_blank_loc, &file_bracket_loc, excludeBrackets);
-        display_lines(fspec, fnames[i], file_loc, file_blank_loc, file_comment_loc, file_bracket_loc, tableOutput, excludeBrackets);
-        file_count++;
-        total_loc += file_loc;
-        total_comment_loc += file_comment_loc;
-        total_blank_loc += file_blank_loc;
-        total_bracket_loc += file_bracket_loc;
+        if ((valid_file = count_loc(fnames[i], &ic, &tf, &p, &count_retcode)) >= 0)
+            display_lines(fspec, fnames[i], &ic, &p);
+        total.loc += ic.loc;
+        total.comment += ic.comment;
+        total.blank += ic.blank;
+        total.bracket += ic.bracket;
+        count++;
     }
-    if (tableOutput)
-    {
-        print_table_separator(longest_fname, excludeBrackets);
-    }
-    display_lines(fspec, "Total lines", total_loc, total_blank_loc, total_comment_loc, total_bracket_loc, tableOutput, excludeBrackets);
+    if (p.tableOutput)
+        print_table_separator(longest_fname, &p);
+
+    if (valid_file >= 0)
+        display_lines(fspec, "Total lines", &total, &p);
     free(fspec);
     free(list_fnames);
     free(fnames);
